2d2c64d35836c5dc4b6797f456a14e364390a02c
cbrussell <chris.russell02@gmail.com>
2022-01-03 14:03:41 -0500

    changes

--- generation/ff.sh
+++ generation/ff.sh
@@ -5,6 +5,6 @@ cd raw || exit
 for i in {1..10}
 do
     cd "${i}" || exit
-    ffmpeg -y -r 24 -start_number 0 -stream_loop 2 -i "${i}_%03d.png"  -c:v libx264 -crf 12 -pix_fmt yuv420p ../../videos/"${i}".mp4 
+    ffmpeg -y -r 24 -start_number 0 -stream_loop 0 -i "${i}_%03d.png"  -c:v libx264 -crf 12 -pix_fmt yuv420p ../../videos/"${i}".mp4 
     cd ..
     done
