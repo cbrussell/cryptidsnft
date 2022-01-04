0ccc5815c15678dccc6e7cec5fd07b1f1d2ef0bc
cbrussell <chris.russell02@gmail.com>
2022-01-04 14:08:48 -0500

    updated manifest with colors

--- generation/names.py
+++ generation/names.py
@@ -2,17 +2,18 @@ import json
 
 f = json.load(open("trait_manifest.json"))
 
-print(f)
+
 data = {}
 
 for a in f:
     # print(a)
     for c in a["categories"]:
         # print(c)
-        for fi in c["traits"]:
+        for fi in c["colors"]:
             # print(fi)
+            for color in fi["traits"]:
 
-            data[fi["trait"]] = ""
+                data[color["trait"]] = color["trait"]
 
 
 with open(f"names33.json", "w") as w:
