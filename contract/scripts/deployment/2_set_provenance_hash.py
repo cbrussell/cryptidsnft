88e979f27c7360a0396b539d691743f0a874495c
cbrussell <chris.russell02@gmail.com>
2022-01-05 01:13:11 -0500

    BIG TEST

--- contract/scripts/deployment/2_set_provenance_hash.py
+++ contract/scripts/deployment/2_set_provenance_hash.py
@@ -8,7 +8,7 @@ def main():
     dev = accounts.add(config['wallets']['from_key'])
     print(dev)
     cryptids = CryptidToken[len(CryptidToken)-1]
-    provenance = 'string to be entered here'
+    provenance = '19d40dc2c4830ac6ee83fd335d393a8fe5d7bca3d8daa54ff863920d4b0f3f8e'
     transaction = cryptids.setProvenanceHash(provenance, {"from": dev})
     print(f'Provenance hash set at: {transaction}')
     provenancehash = cryptids.provenanceHash()
