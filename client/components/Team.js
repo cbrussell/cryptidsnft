2f183287c65ef8c9e675053e7380194cb94ccaf0
cbrussell <chris.russell02@gmail.com>
2022-01-28 08:01:54 -0800

    changes

---
+++ client/components/Team.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Head from "next/head";
+export default function MyApp() {
+
+
+  return (
+    <div id="team">
+      <div className="">
+        Team Members
+      </div>
+    </div>
+  );
+}
