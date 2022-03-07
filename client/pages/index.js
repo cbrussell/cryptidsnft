1605d1a7bf598de48cafc89bd9ddf5659cbaeacd
cbrussell <chris.russell02@gmail.com>
2022-03-07 08:50:42 -0800

    image og

--- client/pages/index.js
+++ client/pages/index.js
@@ -15,6 +15,16 @@ export default function Home() {
         <title>Cryptids</title>
         <meta name="description" content="Part storybook fantasy, part science-fiction. Cryptids is a generative NFT art project of 11,111 unique mythical creatures. Created by @no__solo an @chrisusselljr."/>
         <link rel="icon" href="/favicon.svg" />
+
+     
+        <meta
+          property="og:image"
+          content="https://gateway.pinata.cloud/ipfs/QmThg99oeGNYSHChmbeGbG6282XZBfhgD2zn88Z7PKLdy1"
+        />
+       
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
+
+
         <link
         rel="preload"
         href="/fonts/Exo-Regular.woff"
