38e3c06d8dcbbd8bfe8c4a334f9165e759d8287e
cbrussell <chris.russell02@gmail.com>
2022-03-08 21:32:35 -0800

    meta

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -23,10 +23,10 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Part storybook fantasy, part science-fiction. Cryptids is a generative NFT art project of 11,111 unique mythical creatures. Created by @no__solo an @chrisusselljr." />
         <link rel="icon" href="/favicon.svg" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="http://cryptids.app/" />
+        <meta property="og:url" content="http://www.cryptids.app/" />
         <meta property="og:title" content="Cryptids" />
         <meta property="og:description" content="Part storybook fantasy, part science-fiction. Cryptids is a generative NFT art project of 11,111 unique mythical creatures. Created by @no__solo an @chrisusselljr." />
-        <meta property="og:image" content="https://gateway.pinata.cloud/ipfs/QmThg99oeGNYSHChmbeGbG6282XZBfhgD2zn88Z7PKLdy1" />
+        <meta property="og:image" content="https://gateway.pinata.cloud/ipfs/QmX7YrhWoRzTdbSqU65pxn4pS6tUwTQW9WGMPr35iKDpdn" />
         <script async src="https://cdn.splitbee.io/sb.js"></script>
         <link
           rel="preload"
