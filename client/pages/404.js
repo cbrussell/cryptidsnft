2a41b64c958b9cb58c85b102a3846794aefc2c7d
Chris Russell <chris.russell02@gmail.com>
2022-04-02 16:03:43 -0400

    new

---
+++ client/pages/404.js
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const NotFound = () => {
+    return (
+        <div className="text-center justify-center text-black text-xl mt-8 font-extrabold ">
+            <h1>Oooops...</h1>
+            <h2>That page cannot be found.</h2>
+            <Image 
+            src='/images/1_blank.png'
+            width='300px'
+            height='300px'>
+            </Image>
+            <p>Go back to the <Link href="/"><a className="hover:text-cryptid-6 underline">Homepage</a></Link></p>
+           
+        </div>
+        
+    );
+}
+
+export default NotFound;
