e17d79b36dd726b56c7eae96f742226efb6576fa
Chris Russell <chris.russell02@gmail.com>
2022-06-13 23:16:41 -0700

    merkle

--- contract/scripts/deployment/4_set_merkle_root.py
+++ contract/scripts/deployment/4_set_merkle_root.py
@@ -8,7 +8,7 @@ def main():
     dev = accounts.add(config['wallets']['from_key'])
     print(dev)
     cryptids = Cryptids[len(Cryptids)-1]
-    merkle_root = '0x17861ac73c03a3f144184896b949feb6aea581d0374c0094123470ecefeea308'
+    merkle_root = '0x838184a0f907c9ccc80328fbfb5c87c47186df8c8188db40b54c8a52e6730ec1'
     transaction = cryptids.setMerkleRoot(merkle_root, {"from": dev})
     print(f'Merkle root set at: {transaction}\n')
     print(f'Merkle root set to: {merkle_root}\n')
