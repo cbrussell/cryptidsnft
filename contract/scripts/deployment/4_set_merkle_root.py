ba4209d8aa5ead7c6099a407d4c6f2e38871ff56
Chris Russell <chris.russell02@gmail.com>
2022-06-14 23:09:13 -0700

    merkle

--- contract/scripts/deployment/4_set_merkle_root.py
+++ contract/scripts/deployment/4_set_merkle_root.py
@@ -8,7 +8,7 @@ def main():
     dev = accounts.add(config['wallets']['from_key'])
     print(dev)
     cryptids = Cryptids[len(Cryptids)-1]
-    merkle_root = '0xf160ad9bdd835b745acf369c519085f9474db6e4f636c9b5c5c6cd5818c3bab7'
+    merkle_root = '0x69abcc6717c2817a92a640afbc110f03887be69afacb6c784a7b06dfa663e423'
     transaction = cryptids.setMerkleRoot(merkle_root, {"from": dev})
     print(f'Merkle root set at: {transaction}\n')
     print(f'Merkle root set to: {merkle_root}\n')
--- contract/scripts/deployment/55_airdrop.py
+++ contract/scripts/deployment/55_airdrop.py
@@ -10,17 +10,12 @@ def main():
     print(f'Ruinnng functions as dev: {dev}')
     print(f'Active network is: {active_network}')
 
-    addresses = ['0x932e9A2AFA81c365e375Ffca5349E136046bf97D',
-'0xd3e32b5caa4c5b3ba83f42d24f3ac828159a8efe',
-'0x913D83761Ff529f487623aB6EAdE611DDcda4790',
-'0x8b1b0E98cc5B1FAE2011C2E8a7a527079c763AeC',
-'0xe19558d2b3fabb5c045ddf3b44dc15de18e9cd20',
-'0x1069A65F9346Bd46c2Bd741A5113a2831ffFcce5']
+    addresses = ['0xBe82e4b3F29C71022aB953294ACEBC5Da0a06244']
 
 
 
     cryptids = Cryptids[len(Cryptids)-1]
-    airdrop_amount = 1
+    airdrop_amount = 5
     airdrop_address = "0x27cc5B44FC727f216cdb8a0844Fa433a6117DB87"
 
     for address in addresses:
